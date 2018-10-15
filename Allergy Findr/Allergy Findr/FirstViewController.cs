fac9bb792b0288a0931382a0134133b3279556dd
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-10-15 01:00:57 -0300

    Read barcodes and show ingredients

--- Allergy Findr/Allergy Findr/FirstViewController.cs
+++ Allergy Findr/Allergy Findr/FirstViewController.cs
@@ -1,27 +1,27 @@
 ﻿using System;
 using UIKit;
 using ZXing.Mobile;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace Allergy_Findr
 {
     public partial class FirstViewController : UIViewController
     {
-
-        public FirstViewController (IntPtr handle) : base (handle)
+        public FirstViewController(IntPtr handle) : base(handle)
         {
-
         }
-        public override void ViewDidLoad ()
-        {
-            base.ViewDidLoad ();
 
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
 
             b_Scan.TouchUpInside += ScanButtonClicked;
         }
 
-        public override void DidReceiveMemoryWarning ()
+        public override void DidReceiveMemoryWarning()
         {
-            base.DidReceiveMemoryWarning ();
+            base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
 
@@ -32,8 +32,29 @@ namespace Allergy_Findr
 
             if (result != null)
             {
-                l_BarcodeNumber.Text = result.Text;
+                FindIngredients(result.Text);
             }
+        }
+
+        async void FindIngredients(string barcode)
+        {
+            var client = new HttpClient();
+
+            string url = "https://api.nutritionix.com/v1_1/item?upc=" + barcode + "&appId=1f0ccdae&appKey=45d4c13363d317c3e6a7c0a202dea06c";
+            string json = await client.GetStringAsync(url);
+            RootObject d = JObject.Parse(json).ToObject<RootObject>();
+            DisplayIngredients(d.nf_ingredient_statement);
+        }
+
+        void DisplayIngredients(string content)
+        {
+            UIAlertView alert = new UIAlertView()
+            {
+                Title = "Ingredients Found",
+                Message = content
+            };
+            alert.AddButton("OK");
+            alert.Show();
         }
 
     }
---
+++ Allergy Findr/Allergy Findr/RootObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+public class RootObject
+{
+    public object old_api_id { get; set; }
+    public string item_id { get; set; }
+    public string item_name { get; set; }
+    public object leg_loc_id { get; set; }
+    public string brand_id { get; set; }
+    public string brand_name { get; set; }
+    public object item_description { get; set; }
+    public DateTime updated_at { get; set; }
+    public string nf_ingredient_statement { get; set; }
+    public object nf_water_grams { get; set; }
+    public int nf_calories { get; set; }
+    public int nf_calories_from_fat { get; set; }
+    public int nf_total_fat { get; set; }
+    public int nf_saturated_fat { get; set; }
+    public int nf_trans_fatty_acid { get; set; }
+    public object nf_polyunsaturated_fat { get; set; }
+    public object nf_monounsaturated_fat { get; set; }
+    public int nf_cholesterol { get; set; }
+    public int nf_sodium { get; set; }
+    public int nf_total_carbohydrate { get; set; }
+    public int nf_dietary_fiber { get; set; }
+    public int nf_sugars { get; set; }
+    public int nf_protein { get; set; }
+    public int nf_vitamin_a_dv { get; set; }
+    public int nf_vitamin_c_dv { get; set; }
+    public int nf_calcium_dv { get; set; }
+    public int nf_iron_dv { get; set; }
+    public object nf_refuse_pct { get; set; }
+    public object nf_servings_per_container { get; set; }
+    public double nf_serving_size_qty { get; set; }
+    public string nf_serving_size_unit { get; set; }
+    public object nf_serving_weight_grams { get; set; }
+    public object allergen_contains_milk { get; set; }
+    public object allergen_contains_eggs { get; set; }
+    public object allergen_contains_fish { get; set; }
+    public object allergen_contains_shellfish { get; set; }
+    public object allergen_contains_tree_nuts { get; set; }
+    public object allergen_contains_peanuts { get; set; }
+    public object allergen_contains_wheat { get; set; }
+    public object allergen_contains_soybeans { get; set; }
+    public object allergen_contains_gluten { get; set; }
+    public object usda_fields { get; set; }
+}
